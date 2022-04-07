a3d070305a4bce7f9a8b79b736d5f5042e06517f
Infinite-idea <sinuxion@gmail.com>
2022-04-07 12:01:37 +0430

    libgul updated

--- lib/libgul1.0.0.py
+++ lib/libgul1.0.0.py
@@ -4,8 +4,14 @@ from pathlib import Path as path
 # Main function
 def __main__(FileAddress):
     try:
+        # Open and read urls list file
         Urls = open(FileAddress).read().splitlines()
+
+        # Return urls list
         return Urls
     except Exception as e:
+        # Get error message
         ExcMsg = "Err on: [" + str(path(__file__).name) + "]"
+
+        # Return error message
         return ExcMsg
